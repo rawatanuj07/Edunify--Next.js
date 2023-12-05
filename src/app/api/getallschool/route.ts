eb7b02df23a6fcba097dd1073a60de0d5478a93f
Anuj Rawat <rawatanuj07@gmail.com>
2023-12-05 22:01:18 +0530

    data populated from api successfully

--- src/app/api/getallschool/route.ts
+++ src/app/api/getallschool/route.ts
@@ -4,7 +4,7 @@ import { createDatabasePool } from "@/../db";
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const pool = await createDatabasePool();
-    const [rows] = await pool.execute("SELECT * FROM schools");
+    const rows = await pool.execute("SELECT * FROM schools");
     console.log("the rowss are", rows);
     return NextResponse.json(rows);
   } catch (error) {
--- src/app/screens/retrieve/page.tsx
+++ src/app/screens/retrieve/page.tsx
@@ -1,36 +1,83 @@
-import Image from 'next/image';
-import Link from 'next/link';
 
-function SchoolCard({ school }: any) {
-  // Provide default values if school is undefined
-  const { name = 'rawat', address = 'fbd', city = 'blb' } = school || {};
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState, PropsWithChildren } from "react";
+type CardProps = {
+  id: number;
+  name: string;
+  address: any;
+  city: string;
+  imagePath: any;
+};
+const SchoolCard = ({
+ id, name, address, city,  imagePath,
 
-  // Update the image path based on your project structure
-  const imagePath = `/uploads/me.jpg`; // Adjust the extension as needed
+}: PropsWithChildren<CardProps>) =>{
+  // const { id, name, address, city, imagePath } = school;
+  console.log("Schools IMGare:", imagePath);
 
   return (
-    <div className="relative h-80 w-60 rounded shadow-lg mx-auto border border-gray-300 overflow-hidden">
-      <Link href={`/schools/${name}`} passHref>
-          <div className="h-2/3 relative">
-            <Image
-              src={imagePath}
-              alt={`Image of ${name}`}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className="p-4 flex flex-col justify-between h-1/3">
-            <div>
-              <div className="font-bold text-xl mb-2">{name}</div>
-              <div className="text-gray-700 mb-2">{address}, {city}</div>
-            </div>
-            <div className="flex justify-end">
-              <span className="bg-blue-500 text-white py-1 px-3 rounded-full text-sm">View Details</span>
-            </div>
-          </div>
-      </Link>
-    </div>
+    <Link className="h-80 w-60 rounded shadow-lg mx-auto border border-gray-300 overflow-hidden" href={`/schools/${name}`} passHref>
+      <div className="h-40 relative">
+        <Image
+          src={imagePath}
+          // alt={`Image of ${name}`}
+          alt="IMAGE"
+          layout="fill"
+          objectFit="cover" />
+      </div>
+      <div className="p-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <div className="text-gray-700 mb-2">{address}, {city}</div>
+      </div>
+    </Link>
   );
 }
+interface Schoolss {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  imagePath: any;
+  
+  // other properties...
+}
+export default function Home() {
+  const [schools, setSchools] = useState<Schoolss[]>([]);
 
-export default SchoolCard;
+  useEffect(() => {
+    fetchSchools();
+        // console.log("Schools are:", schools[0]);
+    }, []);
+
+    const fetchSchools = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/getallschool");
+        const data = await res.json();
+        setSchools(data[0]);
+        console.log("Schools fetched:", data[0]);
+  
+        // console.log("SchoolsData fetched:", typeof(schools.name));
+      } catch (error) {
+        console.error("Error fetching schools:", error);
+      }
+    };
+        console.log("Schools aresQ:", schools);
+
+    return (
+      <div>
+      <h1 className="font-medium text-2xl my-4 py-2">Schools</h1>
+      <hr className="mt-1 h-[0.5px] w-full bg-gray-separator tab:mt-2" />
+      <div className="flex flex-wrap">
+        {schools.map((school) => (
+          <div key={school.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-2">
+            <SchoolCard {...school} />
+          </div>
+        ))}
+      </div>
+    </div>
+  
+  );
+    
+}
